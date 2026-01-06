6123055a0d1d17e27ec0c3042426e01a0427afaa
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-06 12:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// your JS code here. If required.
+const sizeInfo = document.getElementById("sizeInfo");
+
+sizeInfo.innerHTML = `<h1>Width: ${window.innerWidth} and Height: ${window.innerHeight}</h1>`;
+
+window.addEventListener('resize', () => {
+  sizeInfo.innerHTML = `<h1>Width: ${window.innerWidth} and Height: ${window.innerHeight}</h1>`;
+});
